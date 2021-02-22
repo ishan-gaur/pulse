db3d12a8876057b332087c6bef5ce4d2aab73be8
Dean Stratakos <dastratakos@gmail.com>
2021-02-21 16:23:53 -0800

    Update manifest and content

--- content.js
+++ content.js
@@ -1,1 +1,23 @@
-alert("Hello from Pulse!")
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+// function postData(input) {
+//   $.ajax({
+//       type: "POST",
+//       url: "/test.py",
+//       data: { param: input },
+//       success: callbackFunc
+//   });
+// }
+
+// function callbackFunc(response) {
+//   // do something with the response
+//   console.log(response);
+// }
+
+// alert("Hello from Pulse!")
+// postData('data to process');
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  postData(request)
+  sendResponse({ classification: "pro" })
+})
