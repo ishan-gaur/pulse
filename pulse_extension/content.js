a72b2cca51ddd410a4e39d05ca318fbd8a594670
Dean Stratakos <dastratakos@gmail.com>
2021-02-21 20:25:27 -0800

    Solve flask call

--- pulse_extension/content.js
+++ pulse_extension/content.js
@@ -1,4 +1,4 @@
-alert("Hello from Pulse!");
+// alert("Hello from Pulse!");
 
 // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //   postData(request)
--- pulse_extension/popup.js
+++ pulse_extension/popup.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   function postData(input) {
-    // const Http = new XMLHttpRequest();
-    const url='localhost:5000/predict';
-    // Http.open("GET", url);
-    // Http.send();
+    const Http = new XMLHttpRequest();
+    const url=`http://localhost:5000/predict?title=${input.title}&target=${input.target}`;
+    Http.open("GET", url);
+    Http.send();
 
-    // Http.onreadystatechange = (e) => {
-    //   console.log(Http.responseText);
-    //   showResponse(Http.responseText);
-    // }
+    Http.onreadystatechange = (e) => {
+      if (Http.readyState === 4) {
+        console.log(Http.responseText);
+        showResponse(Http.responseText);
+      }
+    }
     console.log('in postData');
     // $.ajax({
     //   url: url,
@@ -38,40 +40,32 @@ document.addEventListener('DOMContentLoaded', function () {
   function logSubmit(event) {
     console.log('in logSubmit');
 
-    text = document.getElementById('text').value;
+    title = document.getElementById('title').value;
     target = document.getElementById('target').value;
     timestamp = event.timeStamp;
     
     event.preventDefault();
 
-    // REMOVE because no need to go to a different file
-    // chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-    //   chrome.tabs.sendMessage(
-    //     tabs[0].id,
-    //     { text: text, target: target, time: timestamp },
-    //     showResponse);
-    // });
     console.log('in logSubmit');
-    postData({ text: text, target: target });
-
+    postData({ title: title, target: target });
   }
 
-  function showResponse (res) {
+  function showResponse(res) {
     logTimestamp.textContent = `Time stamp: ${timestamp}`;
-    logtext.textContent = `text: ${text}`;
-    logtarget.textContent = `target: ${target}`;
-    logClassification.textContent = `Classification: ${res}`;
+    logTitle.textContent = `title: ${title}`;
+    logTarget.textContent = `target: ${target}`;
+    logClassification.textContent = `classification: ${res}`;
   }
 
   console.log('charles magic touch');
   
   const form = document.getElementById('form');
   const logTimestamp = document.getElementById('log-timestamp');
-  const logtext = document.getElementById('log-text');
-  const logtarget = document.getElementById('log-target');
+  const logTitle = document.getElementById('log-title');
+  const logTarget = document.getElementById('log-target');
   const logClassification = document.getElementById('log-classification');
 
-  var text = document.getElementById('title').value;
+  var title = document.getElementById('title').value;
   var target = document.getElementById('target').value;
   var timestamp = '';
   
