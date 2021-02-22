d0a4b24a8aba3bd4d419f90cb5b5e43447cbe5a2
Ishan Gaur <ishangaur4@gmail.com>
2021-02-21 20:46:06 -0800

    feedback backend

--- backend/application.py
+++ backend/application.py
@@ -7,20 +7,20 @@ import logging
 from joblib import load
 from model import load_glove, predict, default_save_location
 
-# LOG_FILE = "flask_server.log"
-# if os.path.exists(LOG_FILE):
-   # os.remove(LOG_FILE)
+LOG_FILE = "flask_server.log"
+if os.path.exists(LOG_FILE):
+   os.remove(LOG_FILE)
 
 app = Flask(__name__)
 clf = load(default_save_location)
 load_glove()
 
-# logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format='%(asctime)s %(levelname)s : %(message)s')
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format='%(asctime)s %(levelname)s : %(message)s')
 
 # TODO: shouldn't need this, but commented out just in case
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/predict")
 def predict_sentiment():
@@ -31,3 +31,17 @@ def predict_sentiment():
     sent = "Positive" if predict(clf, title=title, target=target)[0] == 2 else "Negative"
     print("Title: {0}, Target: {1}, Prediction: {2}".format(title, target, sent))
     return sent
+
+# TODO change to POST
+@app.route("/feedback")
+def log_user_feedback():
+    # TODO: error checking for the form entries?
+    title = request.args.get('title')
+    target = request.args.get('target')
+    correct = True if request.args.get('correct') == "Yes" else False
+
+    sent = 1 if predict(clf, title=title, target=target)[0] == 2 else -1
+    if not correct: sent *= -1
+    app.logger.info("Title: {0}, Target: {1}, Correct_Target: {2}".format(title, target, sent))
+    return "recorded"
+
