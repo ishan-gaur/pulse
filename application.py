72ccebb82123dad1c822912d9274b2e5c4ada362
Ishan Gaur <ishangaur4@gmail.com>
2021-03-14 17:59:31 -0700

    Vader final finagling and distillbert initial tests

--- application.py
+++ application.py
@@ -2,9 +2,7 @@ import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 import logging
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from vader import vader_score
 
 
 LOG_FILE = "flask_server.log"
@@ -14,7 +12,6 @@ if os.path.exists(LOG_FILE):
 app = Flask(__name__)
 CORS(app)
 logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format="%(asctime)s %(levelname)s : %(message)s")
-sia = SentimentIntensityAnalyzer()
 
 
 @app.route("/predict")
@@ -23,19 +20,9 @@ def predict_sentiment():
     target = request.args.get("target")
     if not title or not target:
         return "FAILURE, MISSING ARGUMENTS"
-    labels = {"Negative": "neg", "Neutral": "neu", "Positive": "pos"}
-    pol_score = sia.polarity_scores(title)
-    sent = max(labels.keys(), key=(lambda x: pol_score[labels[x]]))
-    print("Title: {0}, Target: {1}, Prediction: {2}".format(title, target, sent))
+    labels = ["Negative", "Neutral", "Positive"]
+    sent = labels[random.randint(0, 2)]
     return sent
-
-def select_polarity(neg, neu, pos):
-    max_score = max([neg, neu, pos])
-    if max_score == neu:
-        if max_score > 0.9:
-            return "Neutral"
-    # TODO add something here to default to neutral if the difference between negative and positive are small?
-    return "Negative" if neg > pos else "Postive"
 
 @app.route("/predict-news")
 def predict_sentiment_news():
@@ -43,11 +30,7 @@ def predict_sentiment_news():
     snippet = request.args.get("snippet")
     if not (title and snippet):
         return "FAILURE, MISSING ARGUMENTS"
-    labels = {"Negative": "neg", "Neutral": "neu", "Positive": "pos"}
-    pol_score = sia.polarity_scores(snippet)
-    sent = select_polarity(pol_score["neg"], pol_score["neu"], pol_score["pos"])
-    print("Title: {0}, Snippet: {1}, Prediction: {2}; Negative: {3}, Neutral: {4}, Positive: {5}".format(title, snippet, 
-        sent, pol_score["neg"], pol_score["neu"], pol_score["pos"]))
+    sent = vader_score(title, snippet)
     return sent
 
 # TODO change to POST
