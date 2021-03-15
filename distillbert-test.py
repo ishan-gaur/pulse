c3c5b6c34907d42e3ed6a6ccbc59abace18fc906
Ishan Gaur <ishangaur4@gmail.com>
2021-03-15 11:29:21 -0700

    distilbert trains but like zero progress so far. mb too little training data, or labels are just wack.

---
+++ distillbert-test.py
@@ -0,0 +1,77 @@
+from collections import Counter
+from datasets import load_dataset
+from transformers import pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.calibration import calibration_curve
+import matplotlib.pyplot as plt
+
+SEED = 12345
+MODEL = "distilbert-base-uncased"
+TRAINED_MODEL = "distilbert-base-uncased-fintuned-persent"
+DATASET = "per_sent"
+
+print("Loading Model")
+classifier = pipeline('sentiment-analysis', model=TRAINED_MODEL, tokenizer=MODEL)
+# labels = {"POSITIVE": 2, "NEUTRAL": 1, "NEGATIVE": 0}
+labels = {"LABEL_1": 1, "LABEL_0": 0}
+
+
+def predict(snippet):
+    output = classifier(snippet)
+    predictions = []
+    for i in range(len(output)):
+        predictions.append(labels[output[i]["label"]])
+        """
+        if abs(output[i]["score"] - 0.5) < 0.15:
+            predictions.append(labels["NEUTRAL"])
+        else:
+            predictions.append(labels[output[i]["label"]])
+        """
+    return predictions
+
+
+def calibration(data):
+    output = classifier(data["input"])
+    print(output)
+    y_pred = []
+    y_true = data["target"]
+    for i in range(len(output)):
+        y_pred.append(output[i]["score"] if output[i]["label"] == "LABEL_1" else (1 - output[i]["score"]))
+    for i in range(len(y_true)):
+        y_true[i] /= 2
+    prob_true, prob_pred = calibration_curve(y_true, y_pred, n_bins=20)
+    plt.plot(prob_pred, prob_true)
+    plt.show()
+
+
+def get_data(dataset):
+    data = {}
+    for key in dataset.keys():
+        input = []
+        target = []
+        for datapoint in dataset[key]:
+            if datapoint["TRUE_SENTIMENT"] == -1: continue
+            if datapoint["TRUE_SENTIMENT"] == 1: continue
+            input.append(datapoint["TITLE"])
+            target.append(datapoint["TRUE_SENTIMENT"])
+        data[key] = {"input": input, "target": target}
+    return data
+
+
+def eval(data):
+    print(Counter(data["target"]))
+    print(accuracy_score(data["target"], [predict(s) for s in data["input"]]))
+
+
+print("Loading data")
+dataset = load_dataset(DATASET)
+data = get_data(dataset)
+"""
+entire_data = {"input": [], "target": []}
+for key in data.keys():
+    for part in data[key].keys():
+        entire_data[part].extend(data[key][part])
+"""
+calibration(data["test_random"])
+eval(data["test_random"])
+print(classifier("Georgia bill is going to be terrible for voters."))
